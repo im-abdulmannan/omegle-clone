f4135a56eef4f635fbefd0f5c82e5af30edb2574
Abdul Mannan <abdulmann1234@gmail.com>
2024-07-18 16:41:50 +0500

    FE basic done

---
+++ frontend/src/components/Landing.tsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+export const Landing = () => {
+  const [name, setName] = useState("");
+  return (
+    <div>
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        placeholder="Enter your name"
+      />
+      <Link
+        to={`/room/?name=${name}`}
+      >
+        Join
+      </Link>
+    </div>
+  );
+};
